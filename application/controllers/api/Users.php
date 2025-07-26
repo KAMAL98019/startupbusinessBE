834758448c97571e554b5be1caa08417e93dbfd1
Ashek-m <ashekksrct@gmail.com>
2025-07-26 11:03:45 +0530

    initial kam guruu

--- application/controllers/api/Users.php
+++ application/controllers/api/Users.php
@@ -16,7 +16,7 @@ class Users extends RestController
     // 📌 Get all users (excluding OTP and passwords)
     public function index_get()
     {
-        $this->db->select("id, username, email, is_verified, created_at, role_id, role_name, notification_enabled");
+        $this->db->select("id, username, email, is_verified, created_at, role_id, role_name, is_accepted, notification_enabled,notification");
         $query = $this->db->get("users");
         $data = $query->result();
         $this->response($data, 200);
@@ -28,7 +28,7 @@ class Users extends RestController
             return $this->response(['status' => false, 'message' => 'Valid User ID is required'], 400);
         }
 
-        $this->db->select("id, username, email, is_verified, created_at, role_id, role_name, notification_enabled");
+        $this->db->select("id, username, email, is_verified, created_at, role_id, role_name, is_accepted ,notification_enabled, notification");
         $this->db->from("users");
         $this->db->where("id", $id);
         $query = $this->db->get();
